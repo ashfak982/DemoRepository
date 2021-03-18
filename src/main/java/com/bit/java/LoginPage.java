b40c21decbe6e24e4a4afcb788655428ddc25462
Ashfak rahman <ashfakrahman@Ashfaks-MacBook-Pro.local>
2021-03-18 01:37:22 -0400

    added search page

---
+++ src/main/java/com/bit/java/LoginPage.java
@@ -0,0 +1,17 @@
+package com.bit.java;
+
+public class LoginPage {
+	
+	
+	public void LoginPage() {
+		
+		System.out.println("LoginPage");
+		
+	}
+	
+public void SearchPage() {
+		
+		System.out.println("SearchPage");
+		
+	}
+}
