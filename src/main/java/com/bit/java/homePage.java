a4fba491c7af0e3e3a6740b4e9ba4db6ee0946ba
Ashfak rahman <ashfakrahman@Ashfaks-MacBook-Pro.local>
2021-03-17 23:43:34 -0400

    my first commit demo for selenium

---
+++ src/main/java/com/bit/java/homePage.java
@@ -0,0 +1,12 @@
+package com.bit.java;
+
+public class homePage {
+  
+	
+	public void homepage() {
+		
+		System.out.println("homepage");
+	}
+	
+	
+}
---
+++ src/test/java/com/bit/test/baseTest.java
@@ -0,0 +1,15 @@
+package com.bit.test;
+
+public class baseTest {
+	
+	
+	
+	
+	public void test1() {
+		
+		System.out.println("test1");
+		
+		
+	}
+
+}
